6fb4eb725c7a6f81ca46a8584b59f649464991c3
Abubakr-Sadik Nii Nai Davis <dwa2pac@gmail.com>
2019-02-21 23:07:34 +0000

    Add ELB for webserver.

--- web.tf
+++ web.tf
@@ -1,6 +1,5 @@
 provider "aws" {
-  "region" = "us-east"
-  "availability_zone" = "1a"
+  region = "us-east-1"
 }
 
 # webserver
@@ -25,13 +24,32 @@ resource "aws_key_pair" "ops" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCoQJU40y0SSKPqOdrcLbIkMXC6fnPEwmmjcKs+AjSc4vAU5ZkSILFJvXIV+UvAqZhEVviqVi3tpB5TWpraFNHiHtneITcZQy3JK71H0Jw9bA5PL9gf7w/Lq4YYDIHEEPU0wseAD3wKU+4HT4wWnaw55i6CMq3ERYfze8c1HD5NOITncZ4jaHMMjTgx8hlNhwYIHRWll7xWA0ExVbr16b/iq7MCX8hPrMg7xzc+Z6k73LTV4WIwfd7ApiGtztqNHdXpLbRcxNDeE24pZ588L8eS6VfFJUeLrxxmxa6BNbEiRtUotbEUyZhTkOq1CA5n6uVaSyMcBvL0DqkbpCqrruuD ops"
 }
 
-
 resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ubuntu_img.id}"
+  availability_zone = "us-east-1a"
   instance_type = "t2.micro"
   key_name = "${aws_key_pair.ops.id}"
   tags = {
     Name = "webserver"
   }
 }
+
+resource "aws_elb" "web-lb" {
+  name               = "web-lb"
+  availability_zones = ["us-east-1a"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  instances                   = ["${aws_instance.web.id}"]
+
+  tags = {
+    Name = "web-lb"
+  }
+}
+
 
